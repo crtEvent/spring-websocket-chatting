e6157abc6315ef09b6f687384e7dbc04cf9cbd6c
Ju, Seonghwan <crtevn@gmail.com>
2024-08-12 20:17:24 +0900

    RoomManage Room 더미 데이터 추가

--- src/main/kotlin/com/example/chat/mvc/service/RoomManager.kt
+++ src/main/kotlin/com/example/chat/mvc/service/RoomManager.kt
@@ -9,6 +9,21 @@ class RoomManager {
 
     private var rooms: MutableMap<String, Room> = LinkedHashMap()
 
+    init {
+        val room1 = Room.create(name = "비밀의 방")
+        val room2 = Room.create(name = "밤하늘 속 대화")
+        val room3 = Room.create(name = "코드 스페이스")
+        val room4 = Room.create(name = "스프링 개발자들")
+        val room5 = Room.create(name = "코틀린 클럽")
+
+        rooms[room1.id] = room1
+        rooms[room2.id] = room2
+        rooms[room3.id] = room3
+        rooms[room4.id] = room4
+        rooms[room5.id] = room5
+    }
+
+
     fun findAll(): List<Room> {
         return ArrayList(rooms.values)
     }
